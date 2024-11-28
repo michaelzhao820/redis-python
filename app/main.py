bad2b9716f38274be1010a878174da21426590c6
Michael Zhao <michaelzhao820@gmail.com>
2024-11-28 17:56:09 -0600

    Threads allow for multiple responses

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
         connection, _ = server_socket.accept()
-        client_thread = threading.Thread(target=handle_connection(), args= (connection,))
+        client_thread = threading.Thread(target=handle_connection, args= (connection,))
         client_thread.start()
 
 def handle_connection(connection):

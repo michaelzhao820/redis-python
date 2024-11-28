692b22fbcef2a14ea33e4f0784572669cdd978a4
Michael Zhao <michaelzhao820@gmail.com>
2024-11-28 13:05:46 -0600

    Always respond with Pong

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import socket  # noqa: F401
 
+from docutils.nodes import address
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,7 +10,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, _ = server_socket.accept()
+    connection.sendall(b"+PONG\r\n")
+
+
 
 
 if __name__ == "__main__":

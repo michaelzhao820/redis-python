b826a31d6800a63c6506ac3bdec543378f31356e
Michael Zhao <michaelzhao820@gmail.com>
2024-11-28 13:12:59 -0600

    naive idea to respond to all commands with Pong

--- app/main.py
+++ app/main.py
@@ -7,9 +7,10 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
+    while True:
+        server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+        connection, _ = server_socket.accept()
+        connection.sendall(b"+PONG\r\n")
 
 
 

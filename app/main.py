05c22e6c42c12dbbedc67f6c0a0f98d206b63238
Michael Zhao <michaelzhao820@gmail.com>
2024-11-28 17:55:10 -0600

    Threads allow for multiple responses

--- app/main.py
+++ app/main.py
@@ -6,10 +6,10 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
         connection, _ = server_socket.accept()
-        client_thread = threading.Thread(target=handleconnection(), args= (connection,))
+        client_thread = threading.Thread(target=handle_connection(), args= (connection,))
         client_thread.start()
 
-def handleconnection(connection):
+def handle_connection(connection):
     with connection:
         while connection.recv(8000):
             connection.sendall(b"+PONG\r\n")

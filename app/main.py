d85ee372ddf500b700faf6bf62596db09c1d3805
Michael Zhao <michaelzhao820@gmail.com>
2024-11-28 17:43:47 -0600

    naive idea to respond to all commands with Pong

--- app/main.py
+++ app/main.py
@@ -5,14 +5,8 @@ def main():
     while True:
         connection, _ = server_socket.accept()
         with connection:
-            while True:
-                data = connection.recv(1024)
-                if not data:
-                    break
-                commands = data.decode().splitlines()
-                for command in commands:
-                    if command.strip().upper() == "PING":
-                        connection.sendall(b"+PONG\r\n")
+            while connection.recv(8000):
+                connection.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()

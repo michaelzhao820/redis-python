6aa3137927d1f990433f9e0c7ecbaa4d143f3b98
Michael Zhao <michaelzhao820@gmail.com>
2024-11-28 17:33:19 -0600

    naive idea to respond to all commands with Pong

--- app/main.py
+++ app/main.py
@@ -2,18 +2,11 @@ import socket  # noqa: F401
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
-        server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
         connection, _ = server_socket.accept()
         connection.sendall(b"+PONG\r\n")
-
-
-
 
 if __name__ == "__main__":
     main()
